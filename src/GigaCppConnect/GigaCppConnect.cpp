41fccbf5f300c19505b4e6cb589f57c3c017e5e9
Alex <nixon2992@gmail.com>
2024-02-09 15:29:51 +0300

    Add base project file

---
+++ src/GigaCppConnect/GigaCppConnect.cpp
@@ -0,0 +1,12 @@
+﻿// GigaCppConnect.cpp: определяет точку входа для приложения.
+//
+
+#include "GigaCppConnect.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
